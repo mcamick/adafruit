5e133358e4b0b49288d22ee70e39f46aff7590d8
Michael A <73963626+mcamick@users.noreply.github.com>
2021-09-05 22:35:17 -0400

    Create time.py
    demonstrates blinking lights using time.sleep function

---
+++ time.py
@@ -0,0 +1,11 @@
+from adafruit_circuitplayground.express import cpx
+import time
+
+while True:
+  if cpx.switch:
+    cpx.pixels.fill( (255, 0, 0) )
+    time.sleep(0.5)
+    cpx.pixels.fill( (0, 0, 0) )
+    time.sleep(0.5)
+  else:
+    cpx.pixels.fill(0, 0, 0)
